9c140e413a2e0a574b3c514ebc2d280efed80278
JordanGallant <39-12602@sae.edu.za>
2023-05-15 12:23:54 +0200

    comit

---
+++ gui.py
@@ -0,0 +1,10 @@
+from logging import root
+
+class DailyDigestGUI:
+    def __init__(self):
+        pass
+
+    if __name__ == "__main__":
+        root = Tk()
+        app = DailyDigestGUI(root)
+        root.mainloop()
